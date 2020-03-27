f5e727842c2562492c4726aeac3d34b564f167d4
Olesya <progafox@gmail.com>
2020-03-27 17:00:14 +0500

    Task 1

---
+++ lesson1/1.py
@@ -0,0 +1,12 @@
+a = 1.5
+b = 4
+c = 'Hello world'
+
+print (a,b,c)
+
+number_int =int(input ('Enter number integer '))
+number_float = float(input ('Enter number float '))
+name = input('Enter your name ')
+
+print (name, number_int, number_float)
+
