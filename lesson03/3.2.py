874ae38618c5c6b6738a51074ee537bc114fd7c7
Olesya <progafox@gmail.com>
2020-04-03 20:35:31 +0500

    Task 3.2

---
+++ lesson03/3.2.py
@@ -0,0 +1,5 @@
+
+def user_data(name, surname, year, city, email, phone):
+    print(f'{name}, {surname},year:{year},city: {city},email:{email}, phone:{phone}')
+
+user_data(name='Ivan', surname='Ivanov', year ='1918', city = 'Moscow', email ='Ivanov_i@mail.ru', phone = '566-567')
